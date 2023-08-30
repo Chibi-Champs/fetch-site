755f6c17c9e2c2c81e7e6daa22c45b517e17315c
netrivas <dcobos00@gmail.com>
2023-08-30 13:59:07 +0000

    second page

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '5e83074ae9msh01a992d70f3dac5p1d8bc3jsn11454281692e',
+		'X-RapidAPI-Key': '319070a8b0msh0d2ddac304140e7p12bd7djsnd62c0587b586',
 		'X-RapidAPI-Host': 'mangaverse-api.p.rapidapi.com'
 	}
 };
@@ -21,9 +21,9 @@ const fetchChapter = async () => {
 
 };
 
-const fetchImg = async () => {
+const fetchImg = async (id) => {
     try {
-	    const response = await fetch(ImagUrl, options);
+	    const response = await fetch(`https://mangaverse-api.p.rapidapi.com/manga/image?id=${id}`, options);
     	const result = await response.json();
 	    console.log(result);
     } catch (error) {
@@ -40,5 +40,11 @@ const fetchMang = async () => {
 	    console.error(error);
     }
 }
+// fetchMang()
+// fetchChapter()
+fetchImg('647fedf7c71c2c9122b31939')
+// console.log(fetchChapter(), fetchImg(fetchMang().data[0].id))
 
-console.log(fetchMang(), fetchChapter(), fetchImg());
+let chapter = document.querySelector('#chapter')
+let page = document.createElement('img')
+
