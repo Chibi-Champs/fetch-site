cce14967d9d1c6836d2bcc97b2fd02a4336a7753
Joeyyyys <JosephGiler2@gmail.com>
2023-08-30 02:08:56 +0000

    added a function that loops through our fetch call of chapter to display and append the image previews withe title names, also added some css styling to it

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
     
-    console.log(apiKey);
+console.log(apiKey);
     
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '5e83074ae9msh01a992d70f3dac5p1d8bc3jsn11454281692e',
+		'X-RapidAPI-Key': '319070a8b0msh0d2ddac304140e7p12bd7djsnd62c0587b586',
 		'X-RapidAPI-Host': 'mangaverse-api.p.rapidapi.com'
 	}
 };
@@ -31,14 +31,46 @@ const fetchImg = async () => {
     }
 }
 
-const fetchMang = async () => {
+// const fetchMang = async () => {
+//     try {
+// 	    const response = await fetch(apiKey, options);
+// 	    const result = await response.json();
+// 	    console.log(result);
+//     } catch (error) {
+// 	    console.error(error);
+//     }
+// }
+const createMangaThumbnails = (thumbnail, title) => {
+	const ul = document.querySelector("#mangaList");
+	const li = document.createElement("li");
+	const img = document.createElement("img");
+	const h3 = document.createElement("h3");
+	
+	li.className = "mangaPreview";
+	img.className = "img";
+	h3.className = "previewH3";
+	img.src = thumbnail;
+	h3.textContent = title;
+	li.append(img, h3);
+	ul.append(li);
+}
+
+const displayManga = async () => {
     try {
 	    const response = await fetch(apiKey, options);
 	    const result = await response.json();
-	    console.log(result);
+	    let data = result.data
+	    for (let i = 0; i < data.length; i++) {
+	    	createMangaThumbnails(data[i].thumb, data[i].title)
+	    	console.log(result);	
+	    }
     } catch (error) {
 	    console.error(error);
     }
 }
 
-console.log(fetchMang(), fetchChapter(), fetchImg());
+
+console.log(fetchChapter(), displayManga(), fetchImg());
+
+
+
