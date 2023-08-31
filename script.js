445c55b143d8fd67ae4606cce89655eccdb94260
netrivas <dcobos00@gmail.com>
2023-08-31 16:53:48 +0000

    dialog el

--- script.js
+++ script.js
@@ -23,46 +23,28 @@ const createMangaThumbnails = (thumbnail, title, id) => {
 	img.src = thumbnail;
 	h3.textContent = title;
 	
-	li.addEventListener('click', e => {
-		let description = document.createElement('p')
-		fetch(`${mangaKey}/${id}/moreinfo`)
-		.then(res => res.json())
-		.then(info => {
-			description.textContent = info.data.moreinfo
-			console.log(info.data.moreinfo)
-		})
-		let div = document.createElement('div')
-		div.append(description)
-		// document.body.append(div)
-	
+	li.addEventListener('click', async e => {
+		try{
+			const response = await fetch(`${mangaKey}/${id}/moreinfo`);
+        	const info = await response.json();
+        	
+			const closeButton = document.getElementById('closeDialog')
+			const dialog = document.getElementById('moreInfoDialog');
+            const moreInfoText = document.getElementById('moreInfoText')
+            
+            moreInfoText.textContent = info.data.moreinfo;
+            dialog.showModal()
+            closeButton.addEventListener('click', () => {
+                dialog.close();
+            });
+		}catch (error) {
+            console.error(error);
+        }
 		
 	})
 	
 	li.append(img, h3);
 	ul.append(li);
-	
-	 li.addEventListener('click', async e => {
-        try {
-            const response = await fetch(`${mangaKey}/${id}/moreinfo`);
-            const info = await response.json();
-
-            // Get a reference to the dialog element and the text element inside it
-            const dialog = document.getElementById('moreInfoDialog');
-            const moreInfoText = document.getElementById('moreInfoText');
-
-            // Set the text content of the dialog and open it
-            moreInfoText.textContent = info.data.moreinfo;
-            dialog.showModal();
-
-            // Handle the close button
-            const closeButton = document.getElementById('closeDialog');
-            closeButton.addEventListener('click', () => {
-                dialog.close();
-            });
-        } catch (error) {
-            console.error(error);
-        }
-    });
 }
 
 
@@ -73,7 +55,7 @@ const displayManga = async () => {
 	    const result = await response.json();
 	    let data = result.data
 	    for (let i = 0; i < data.length; i++) {
-	    	createMangaThumbnails(data[i].images.jpg.image_url, data[i].title, data[i].mal_id);	
+	    	createMangaThumbnails(data[i].images.jpg.image_url, data[i].title, data[i].mal_id, );	
 	    }
     } catch (error) {
 	    console.error(error);
